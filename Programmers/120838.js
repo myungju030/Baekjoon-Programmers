71a26cdd1aa80a233a269d4d3a97ca13228c94d4
myungju <myungju030@gmail.com>
2022-10-30 14:40:04 +0900

    add: 20221030 코테 풀이

--- Programmers/120838.js
+++ Programmers/120838.js
@@ -1,15 +1,27 @@
 //모스부호(1)
+// function solution(letter) {
+//     var answer = '';
+//     let morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+//     let arr=letter.split(' ');
+//     for(let i=0; i<arr.length; i++){
+//         for(let j=0; j<morse.length; j++){
+//             if(arr[i]===morse[j]){
+//                 answer = answer + String.fromCodePoint(j+97)
+//             }
+//         }
+        
+//     }
+//     return answer;
+// }
+
 function solution(letter) {
     var answer = '';
-    let morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-    let arr=letter.split(' ');
-    for(let i=0; i<arr.length; i++){
-        for(let j=0; j<morse.length; j++){
-            if(arr[i]===morse[j]){
-                answer = answer + String.fromCodePoint(j+97)
-            }
-        }
-        
-    }
-    return answer;
+    let morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+    return letter.split(' ').map(x=>morse[x]).join('');
 }
