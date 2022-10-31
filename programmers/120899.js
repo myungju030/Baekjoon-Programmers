2a908aa56bcc170c9a0d2bd2f1adf312d6cd2bff
myungju <myungju030@gmail.com>
2022-10-31 14:49:54 +0900

    add: 20221031 코테 풀이

---
+++ programmers/120899.js
@@ -0,0 +1,12 @@
+//가장 큰 수 찾기
+function solution(array) {
+    let max=0;
+    let index=0;
+    for(let i=0; i<array.length; i++){
+        if(array[i]>max){
+            max=array[i];
+            index=i;
+        }
+    }
+    return [max, index];
+}
