1613efa8ffe32112a1fffacd8da3feb0bd76b2fe
myungju <myungju030@gmail.com>
2022-11-29 19:25:17 +0900

    add: 12912 두 정수 사이의 합

---
+++ programmers/level_1/12912.js
@@ -0,0 +1,10 @@
+// 두 정수 사이의 합
+function solution(a, b) {
+    var answer = 0;
+    let max=Math.max(a,b);
+    let min=Math.min(a,b);
+    for(let i=min; i<=max; i++){
+        answer +=i;
+    }
+    return answer;
+}
