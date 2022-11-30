ff59b8788ca5ab3387b08d63a3dd8eb143e2b427
myungju <myungju030@gmail.com>
2022-11-30 22:59:34 +0900

    add: 76501 음양 더하기

---
+++ programmers/level_1/76501.js
@@ -0,0 +1,8 @@
+// 음양 더하기
+function solution(absolutes, signs) {
+    var answer = 0;
+    for(let i=0; i<absolutes.length; i++){
+        signs[i] ? answer += absolutes[i] : answer -= absolutes[i] 
+    }
+    return answer;
+}
