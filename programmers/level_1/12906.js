56b9b8c0a83cb8bf772099aa6cdbf6d9b9041bb5
myungju <myungju030@gmail.com>
2022-12-01 01:53:42 +0900

    mod: 12906 같은 숫자는 싫어

--- programmers/level_1/12906.js
+++ programmers/level_1/12906.js
@@ -1,11 +1,4 @@
 // 같은 숫자는 싫어
-function solution(arr)
-{
-    var answer = [];
-    for(let i=0; i<arr.length-1; i++){
-        if(arr[i]===arr[i+1]){
-            arr[i]=null;
-        }
-    }
-    return arr.filter(x=>x!==null);
-}
+function solution(arr){
+    return arr.filter((el,idx)=> el !== arr[idx+1]);
+ }
