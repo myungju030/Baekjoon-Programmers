aeca0630864a2864b30103306fba3527a3f5f3a2
myungju <myungju030@gmail.com>
2023-01-02 02:33:08 +0900

    mod: 92334 신고 결과 받기

--- programmers/level_1/92334.js
+++ programmers/level_1/92334.js
@@ -1,25 +1,31 @@
 // 신고 결과 받기
 function solution(id_list, report, k) {
-  const answer = new Array(id_list.length).fill(0);
-  const report_list = {}; //
+  let obj = {}; // {유저 ID: [유저가 신고한 ID],~~}
+  let reportObj = {}; // {유저가 신고한 ID: 신고받은 횟수,~~}
+  let reportArr = report.map((x) => x.split(" ")); // [[유저ID, 유저가 신고한 ID],~~]
+  let stop = []; // 정지 ID
 
-  id_list.map((user) => {
-    report_list[user] = [];
+  // [유저ID:[],~~]
+  id_list.forEach((x) => {
+    obj[x] = Array(0);
   });
 
-  report.map((user) => {
-    const [user_id, report_id] = user.split(" ");
-    if (!report_list[report_id].includes(user_id)) {
-      report_list[report_id].push(user_id);
+  reportArr.forEach((x) => {
+    if (!obj[x[0]].includes(x[1])) {
+      // 유저가 신고한 ID가 중복되지않을 때
+      obj[x[0]].push(x[1]); // 유저가 신고한 ID 배열에 추가
+      reportObj[x[1]] = reportObj[x[1]] + 1 || 1; // 신고한 ID의 신고받은 횟수 추가
+      // k번 이상 신고된 유저 중복되지않게 stop배열에 추가
+      if (reportObj[x[1]] >= k && !stop.includes(reportObj[x[1]]))
+        stop.push(x[1]);
     }
   });
 
-  for (const key in report_list) {
-    if (report_list[key].length >= k) {
-      report_list[key].map((user) => {
-        answer[id_list.indexOf(user)] += 1;
-      });
-    }
+  // 각각 유저가 신고한 ID에서 k번 이상 신고된 유저만 남기기
+  for (const key in obj) {
+    obj[key] = obj[key].filter((x) => stop.includes(x));
   }
-  return answer;
+
+  // 각각 유저가 신고한 ID에서 k번 이상 신고된 유저만 남겨진 배열의 길이 출력
+  return Object.values(obj).map((x) => x.length);
 }
