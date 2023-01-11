e4824e931976962f73eeab627372692a9d67772f
myungju <myungju030@gmail.com>
2023-01-11 23:24:44 +0900

    add: 12973 짝지어 제어하기

---
+++ programmers/level_2/12973.js
@@ -0,0 +1,15 @@
+// 짝지어 제어하기
+function solution(s) {
+  var answer = -1;
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    arr.push(s[i]);
+
+    if (arr.length > 1 && arr[arr.length - 1] === arr[arr.length - 2]) {
+      arr.pop();
+      arr.pop();
+    }
+  }
+
+  return arr.length === 0 ? 1 : 0;
+}
