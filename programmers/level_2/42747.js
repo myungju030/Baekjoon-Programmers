1f41c4e4a36a710d12ce71713ae7a1a07bedc207
myungju <myungju030@gmail.com>
2023-01-20 12:45:24 +0000

    add: 42747 H-Index

---
+++ programmers/level_2/42747.js
@@ -0,0 +1,20 @@
+// H-Index
+function solution(citations) {
+    let index = 1; // 논문의 수 : 1편부터 시작
+
+    // 내림차순으로 정렬
+    citations.sort((a, b) => b - a);
+
+    for (let el of citations) {
+        if (el < index) { // 논문의 인용 횟수가 논문의 수보다 작으면 직전의 index가 최댓값
+            return index - 1; // 직전 index 반환
+        }
+        else if (el === index) { // 논문의 인용 횟수와 논문의 수가 같으면 해당 index가 최댓값
+            return index; // 해당 index 반환
+        }
+        index++; // 논문의 인용 횟수가 논문의 수보다 크면 논문의 수 증가
+    }
+
+    // 모든 논문의 인용 횟수가 논문의 수 이상일 때 논문의 수 반환
+    return citations.length;
+}
